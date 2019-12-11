ebfc8487732cb34e2b558e29c6345a3cbe431388
fbaumgart <f.m.baumgart@hotmail.com>
2019-12-11 22:42:26 +0100

    First commit

---
+++ app/src/main/java/com/example/gymtracker/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.gymtracker
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val newTrainingBtn: Button = findViewById(R.id.newTrainingButton)
+        newTrainingBtn.setOnClickListener {
+            val intent = Intent(this, NewTraining::class.java)
+            startActivity(intent)
+        }
+    }
+}
+
+
---
+++ app/src/main/java/com/example/gymtracker/NewTraining.kt
@@ -0,0 +1,26 @@
+package com.example.gymtracker
+
+import android.os.Build
+import android.os.Bundle
+import android.support.annotation.RequiresApi
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_newtraining.*
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+class NewTraining : AppCompatActivity(){
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_newtraining)
+
+        val currentDateTime = LocalDateTime.now()
+        val formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy    HH:mm")
+        val formattedCurrentDateTime = currentDateTime.format(formatter)
+
+        var newTrainingDayTimeTextView : TextView = findViewById<TextView>(R.id.newTrainingDayTime)
+        newTrainingDayTimeTextView.append("    " + formattedCurrentDateTime)
+    }
+}
